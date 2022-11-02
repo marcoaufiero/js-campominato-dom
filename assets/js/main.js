4225e2efe6f78caef3c94defeed65ef9cc81d93f
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-02 12:05:19 +0100

    difficulty level setting added

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,66 @@ function squareGenerator(){
     return div;
 }
 
+
 function play(){
+
+let difficultyLevel = parseInt(document.getElementById('difficulty').value);
+
+console.log(difficultyLevel);
+
+if(difficultyLevel === 1){
 
     grid.innerHTML = '';
 
     for(let i = 0; i < 100; i++){
 
         let activeSquare = squareGenerator();
+
+        activeSquare.classList.add('squareEasy');
+        activeSquare.innerText = i + 1;
+    
+        activeSquare.addEventListener('click', function(){
+            this.classList.add('active');
+            
+            console.log(`Quadrato numero: ${activeSquare.innerText}`)
+        })
+    
+
+
+        grid.append( activeSquare);
+    
+    }
+
+}else if(difficultyLevel === 2){
+    
+    grid.innerHTML = '';
+
+    for(let i = 0; i < 81; i++){
+
+        let activeSquare = squareGenerator();
+        activeSquare.classList.add('squareMedium');
+
+        activeSquare.innerText = i + 1;
+    
+        activeSquare.addEventListener('click', function(){
+            this.classList.add('active');
+            console.log(`Quadrato numero: ${activeSquare.innerText}`)
+        })
+    
+
+
+        grid.append( activeSquare);
+    
+    }
+    
+}else{
+
+    grid.innerHTML = '';
+
+    for(let i = 0; i < 49; i++){
+
+        let activeSquare = squareGenerator();
+        activeSquare.classList.add('squareHard');
 
         activeSquare.innerText = i + 1;
     
@@ -31,3 +84,4 @@ function play(){
 }
 
 
+}
